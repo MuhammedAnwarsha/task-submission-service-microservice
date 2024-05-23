f95bd5a03dd77913522564db972f3e525080b918
MuhammedAnwarsha <muhammedanwarshab@gmaill.com>
2024-05-23 21:44:36 +0530

    Initial commit

---
+++ src/main/java/com/task/modal/UserDto.java
@@ -0,0 +1,20 @@
+package com.task.modal;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDto {
+
+    private Long id;
+
+    private String email;
+
+    private String password;
+
+    private String role;
+
+    private String fullName;
+}
---
+++ src/main/java/com/task/service/TaskService.java
@@ -0,0 +1,20 @@
+package com.task.service;
+
+import com.task.modal.TaskDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+@FeignClient(name = "TASK-SERVICE", url = "http://localhost:5002")
+public interface TaskService {
+
+    @GetMapping("/api/tasks/{id}")
+    public TaskDto getTaskById(@PathVariable Long id,
+                               @RequestHeader("Authorization")String jwt) throws Exception;
+
+    @PutMapping("/api/tasks/{id}/complete")
+    public TaskDto completeTask(@PathVariable Long id) throws Exception;
+}
